1b8389b46fa9e0d4b466d6873b5820920b3aafb0
maalvares <maatheusalvares@gmail.com>
2025-03-04 19:48:14 +0000

    Desafio Xadrez Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,7 @@
 #include <stdio.h> 
 
 int main(){
+    int i = 0, j;
     //Mover a torre 5 casas para a direita
     printf("Movimentação da Torre: \n");    
     for (int t = 0; t < 5; t++){
@@ -17,7 +18,20 @@ int main(){
     printf("\nMovimentação da Rainha: \n");    
     for (int r = 0; r < 8; r++){
         printf("Esquerda.\n"); //imprime a direção do movimento de acordo com a quantidade de repetições necessárias
-    }    
+    }   
+
+    //Mover o cavalo dois para baixo e um para a esquerda
+    printf("\nMovimentação do Cavalo: \n");
+    
+    // loop para garantir a movimentação para a esquerda
+    do{ 
+        // loop para a garantir duas movimentações para baixo
+        for (j = 0; j < 2; j++){ 
+            printf("Baixo.\n");
+        }
+        printf("Esquerda.\n");
+        i++;        
+    }while (i < 1);    
 
     return 0;
 }   
